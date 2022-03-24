fdf80e009aeb28e6f1a5c3ba67c482f710eff4b6
Flav (Lotus) <flaviene.scheidt@uni.lu>
2022-03-24 19:22:56 +0100

    Logics for connecting the peers using the underlying mesh we want

--- clusterManager.go
+++ clusterManager.go
@@ -1,11 +1,32 @@
- package main
+package main
 
 import (
     "bufio"
     "fmt"
-    "io"
+    "io/ioutil"
+    "log"
     "os"
+    "strings"
+    "strconv"
+    
+    "github.com/libp2p/go-libp2p-core/crypto"
+    "github.com/libp2p/go-libp2p-core/peer"
+    "io"
+    // "log"
+    mrand "math/rand"
 )
+
+type peerInfo struct {
+    name string
+    ip string
+    id int
+}
+
+type nodeInfo struct {
+    name string
+    id int
+    peersList []peerInfo
+}
 
 func check(e error) {
     if e != nil {
@@ -13,7 +34,91 @@ func check(e error) {
     }
 }
 
-func getNodeName()(nodeName string)
-{
+func newNode()(nodeInfo) {
+    var thisNode nodeInfo;
 
+    //get node name from /etc/hostname
+    nodeName, err := ioutil.ReadFile("/etc/hostname")
+    if err != nil {
+        log.Fatal(err)
+    }
+    thisNode.name =  strings.TrimSpace(fmt.Sprintf("%s",nodeName))
+
+    //get node id from clusterConfig.csv
+    clusterConfig, err := os.Open("clusterConfig.csv")
+    if err != nil {
+        log.Fatal(err)
+    }
+    // defer clusterConfig.Close()
+
+    //Read line by line to get the info from the csv
+    scanner := bufio.NewScanner(clusterConfig)
+        if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+    searchString := ","+thisNode.name+","
+    for scanner.Scan() {
+        if strings.Contains(scanner.Text(), searchString) {
+            dataExtract := strings.Split(scanner.Text(), ",")
+            thisNode.id, err = strconv.Atoi(dataExtract[2])
+        }
+    }
+
+    //Creates list of peers
+    unl, err := os.Open("./clusterConfig/"+thisNode.name+".txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer unl.Close()
+    scanner = bufio.NewScanner(unl)
+        if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+    for scanner.Scan() {
+        thisNode.peersList = append(thisNode.peersList, peerInfo{strings.TrimSpace(scanner.Text()), "", 0})
+    }
+
+    //Inserts info about every peer in the list
+    for j := 0; j<len(thisNode.peersList); j++ {
+        searchString := ","+thisNode.peersList[j].name+","
+        clusterConfig, err := os.Open("clusterConfig.csv")
+        if err != nil {
+            log.Fatal(err)
+        }
+        scanner = bufio.NewScanner(clusterConfig)
+        if err := scanner.Err(); err != nil {
+            log.Fatal(err)
+        }
+
+        for scanner.Scan() {
+            if strings.Contains(scanner.Text(), searchString) {
+                dataExtract := strings.Split(scanner.Text(), ",")
+                thisNode.peersList[j].id, err = strconv.Atoi(dataExtract[2])
+                thisNode.peersList[j].ip = dataExtract[0]
+            }
+        }
+
+    }
+
+    return thisNode
+}
+
+func getPeerMultAddr(peerElement peerInfo)(string) {
+     var seed io.Reader
+
+    //Use the name of the node to derive the key. Always derive the same key
+    seed = mrand.New(mrand.NewSource(int64(peerElement.id)))
+    prvKey, _, err := crypto.GenerateKeyPairWithReader(crypto.Ed25519, 2048, seed)
+    if err != nil {
+        log.Println(err)
+        return ""
+    }
+
+    peerID, err := peer.IDFromPublicKey(prvKey.GetPublic())
+    pID := fmt.Sprintf("%s", peerID)
+
+    mAddr := "/ip4/"+peerElement.ip+"/tcp/45511/p2p/"+pID
+    return mAddr
 }
---
+++ libp2pManager.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+    "context"
+    // "encoding/binary"
+    // "flag"
+    "fmt"
+    "os"
+    // "os/signal"
+    // "syscall"
+    // "time"
+
+    "github.com/libp2p/go-libp2p"
+    "github.com/libp2p/go-libp2p-core/host"
+    "github.com/libp2p/go-libp2p-core/network"
+    "github.com/libp2p/go-libp2p-core/peer"
+    // "github.com/libp2p/go-libp2p-discovery"
+    "github.com/multiformats/go-multiaddr"
+
+    // "crypto/rand"
+    "bufio"
+    "io"
+    "log"
+    // mrand "math/rand"
+
+    "github.com/libp2p/go-libp2p-core/crypto"
+    "github.com/libp2p/go-libp2p-core/peerstore"
+    // pubsub "github.com/libp2p/go-libp2p-pubsub"
+
+)
+
+func readData(rw *bufio.ReadWriter) {
+    for {
+        str, _ := rw.ReadString('\n')
+
+        if str == "" {
+            return
+        }
+        if str != "\n" {
+            // Green console colour:    \x1b[32m
+            // Reset console colour:    \x1b[0m
+            fmt.Printf("\x1b[32m%s\x1b[0m> ", str)
+        }
+
+    }
+}
+
+func writeData(rw *bufio.ReadWriter) {
+    stdReader := bufio.NewReader(os.Stdin)
+
+    for {
+        fmt.Print("> ")
+        sendData, err := stdReader.ReadString('\n')
+        if err != nil {
+            log.Println(err)
+            return
+        }
+
+        rw.WriteString(fmt.Sprintf("%s\n", sendData))
+        rw.Flush()
+    }
+}
+// const protocolID = "/xrpl/1.0.0"
+// const discoveryNamespace = "xrpl"
+
+//Start peer and wait for icoming connections
+//We are not veirfying if the node is on the list or not because we are using a controled test environment
+//  This environment has programmed tasks to guarantee that everything will go accordinly
+func startPeer(ctx context.Context, h host.Host, streamHandler network.StreamHandler) {
+    // Set a function as stream handler.
+    // This function is called when a peer connects, and starts a stream with this protocol.
+    // Only applies on the receiving side.
+    h.SetStreamHandler("/xrpl/1.0.0", streamHandler)
+
+    // Let's get the actual TCP port from our listen multiaddr, in case we're using 0 (default; random available port).
+    var port string
+    for _, la := range h.Network().ListenAddresses() {
+        if p, err := la.ValueForProtocol(multiaddr.P_TCP); err == nil {
+            port = p
+            break
+        }
+    }
+
+    if port == "" {
+        log.Println("was not able to find actual local port")
+        return
+    }
+
+    log.Println("No one online... Waiting for incoming connection")
+    log.Println()
+}
+
+//Habndle streams after receiving an incoming transmission
+func handleStream(s network.Stream) {
+    log.Println("Got a new stream!")
+
+    // Create a buffer stream for non blocking read and write.
+    rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+
+    go readData(rw)
+    go writeData(rw)
+
+    // stream 's' will stay open until you close it (or the other side closes it).
+}
+
+//Creates the libp2p layer
+func makeHost(port int, seed io.Reader) (host.Host, error) {
+    // Creates a new ED25519 key pair for this host.
+    // Using ed25519 instead of RSA because RSA implementation in go prevents deterministic behavior
+    // We need deterministic behavior, since it is a test enviroment
+    // REMEMBER TO USE RSA WITH A RANDOM SEED IN PRODUCTION
+    prvKey, _, err := crypto.GenerateKeyPairWithReader(crypto.Ed25519, 2048, seed)
+    if err != nil {
+        log.Println(err)
+        return nil, err
+    }
+
+    // 0.0.0.0 will listen on any interface device.
+    sourceMultiAddr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", port))
+
+    // libp2p.New constructs a new libp2p Host.
+    // Other options can be added here.
+    return libp2p.New(
+        libp2p.ListenAddrs(sourceMultiAddr),
+        libp2p.Identity(prvKey),
+    )
+}
+
+//Start peert and connect to the list =)
+func startPeerAndConnect(ctx context.Context, h host.Host, destination string) (*bufio.ReadWriter, error) {
+    // log.Println("This node's multiaddresses:")
+    // for _, la := range h.Addrs() {
+    //     log.Printf(" - %v\n", la)
+    // }
+    // log.Println()
+
+    // Turn the destination into a multiaddr.
+    maddr, err := multiaddr.NewMultiaddr(destination)
+    if err != nil {
+        log.Println(err)
+        return nil, err
+    }
+
+    // Extract the peer ID from the multiaddr.
+    info, err := peer.AddrInfoFromP2pAddr(maddr)
+    if err != nil {
+        log.Println(err)
+        return nil, err
+    }
+
+    // Add the destination's peer multiaddress in the peerstore.
+    // This will be used during connection and stream creation by libp2p.
+    h.Peerstore().AddAddrs(info.ID, info.Addrs, peerstore.PermanentAddrTTL)
+
+    // Start a stream with the destination.
+    // Multiaddress of the destination peer is fetched from the peerstore using 'peerId'.
+    s, err := h.NewStream(context.Background(), info.ID, "/xrpl/1.0.0")
+    if err != nil {
+        log.Println(err)
+        return nil, err
+    }
+    log.Println("Established connection to destination")
+
+    // Create a buffered stream so that read and writes are non blocking.
+    rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+
+    return rw, nil
+}
